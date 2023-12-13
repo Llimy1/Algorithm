d3b27713d49e5a7e2fc5b4f32ad39f311f69e2c8
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2023-12-13 22:48:35 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for (int k : numbers) {
+            answer += k;
+            if (answer > n) break;
+        }
+        return answer;
+    }
+}
