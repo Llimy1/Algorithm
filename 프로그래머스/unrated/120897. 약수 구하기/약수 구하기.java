3a0896204e272b4d648346f7aef03d30be98f7c4
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2023-12-17 15:48:27 +0900

    [level unrated] Title: 약수 구하기, Time: 0.19 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                arr.add(i);
+            }
+        }
+        Collections.sort(arr);
+        int[] answer = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i).intValue();
+        }
+        
+        return answer;
+    }
+}
