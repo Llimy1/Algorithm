52d6e71a4527f68dc591f895828f25f7f58905e7
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2023-12-28 11:23:11 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 5.13 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        String lowerStr = myString.toLowerCase();
+        String lowerPat = pat.toLowerCase();
+        
+        return lowerStr.contains(lowerPat) ? 1 : 0;
+    }
+}
