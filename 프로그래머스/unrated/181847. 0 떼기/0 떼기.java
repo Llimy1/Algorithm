25ed2a2330da28a3478e9318d885307927011a6a
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2024-01-02 11:22:41 +0900

    [level unrated] Title: 0 떼기, Time: 0.01 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String n_str) {        
+        if (n_str.startsWith("0")) {
+            int index = 0;
+            for (int i = 0; i < n_str.length(); i++) {
+                if (n_str.charAt(i) != '0') {
+                    index = i;
+                    break;
+                }
+            }
+            return n_str.substring(index);
+            
+        } else {
+            return n_str;
+        }
+    }
+}
