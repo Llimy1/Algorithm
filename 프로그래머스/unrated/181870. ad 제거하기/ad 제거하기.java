a8bf07b63fc5df57c7b68374a11fb55e946fbfa6
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2024-01-03 11:11:11 +0900

    [level unrated] Title: ad 제거하기, Time: 0.99 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList<String> solution(String[] strArr) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+                answer.add(str);
+            }
+        }
+        return answer;
+    }
+}
