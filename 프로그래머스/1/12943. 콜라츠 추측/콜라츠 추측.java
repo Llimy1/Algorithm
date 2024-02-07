73704ab86a85bb9cbb3641bb925c3377eafe5d94
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2024-02-07 12:18:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else if(num % 2 == 1) {
+                num = (num * 3) + 1;
+            } else {
+                break;
+            }
+            answer++;
+        }
+        
+        if (num != 1) {
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}
