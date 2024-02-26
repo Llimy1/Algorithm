b204bc6b93df2055acce88dae765b72b9021a702
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2024-02-26 23:28:33 +0900

    [level 0] Title: 세 개의 구분자, Time: 117.44 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String myStr) {
+        
+        myStr = myStr.replaceAll("a", " ");
+        myStr = myStr.replaceAll("b", " ");
+        myStr = myStr.replaceAll("c", " ");
+        
+        String[] sArr = myStr.split("\\s*\\s");
+        
+        ArrayList<String> list = new ArrayList<>();
+        if (sArr.length == 0) {
+            list.add("EMPTY");
+        }
+        for (String s : sArr) {
+            list.add(s);                
+        }
+        
+        list.removeAll(Arrays.asList(""));
+        String[] answer = new String[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
