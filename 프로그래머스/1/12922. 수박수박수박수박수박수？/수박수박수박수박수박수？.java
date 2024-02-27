08542e2075364e7af5928647771d20396fb1a0e5
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2024-02-27 23:31:50 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.11 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder answer = new StringBuilder();
-        
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                answer.append("수");
-            } else {
-                answer.append("박");
-            }
-        }
-        
-        return answer.toString();
+       return new String(new char [n/2+1]).replace("\0", "수박").substring(0,n);
     }
 }
